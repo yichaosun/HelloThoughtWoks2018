f195ed696202f4489fff388fdb86c3eba9ccde17
Yichao Sun <sunyichao_believe@me.com>
2017-09-12 06:50:39 +0800

    yichao: init Constants for responseCode

---
+++ src/main/java/com/yichao/thoughtworks/homework/badminton/common/Constants.java
@@ -0,0 +1,11 @@
+package com.yichao.thoughtworks.homework.badminton.common;
+
+public class Constants {
+    public static final class ResponseCode {
+        public static final int NORMAL = 0;
+        public static final int BOOK_ACCEPTED = 1;
+        public static final int BOOK_INVALID = 2;
+        public static final int BOOK_CONFLICTS = 3;
+        public static final int BOOK_CANCELLED_DOES_NOT_EXIT = 4;
+    }
+}
